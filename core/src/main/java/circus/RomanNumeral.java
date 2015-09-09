fa03593c13daded5ca2bae405e5970678c84becf
Andreas Lindeboom <andreaslindeboom@mdmail.nl>
2015-09-09 12:51:10 +0200

    BREAKING THE TEST NAO

--- core/src/main/java/circus/RomanNumeral.java
+++ core/src/main/java/circus/RomanNumeral.java
@@ -97,13 +97,13 @@ public class RomanNumeral {
      */
     private int letterToNumber(char letter) {
         switch (letter) {
-            case 'I':  return 1;
-            case 'V':  return 5;
-            case 'X':  return 10;
-            case 'L':  return 50;
-            case 'C':  return 100;
-            case 'D':  return 500;
-            case 'M':  return 1000;
+            case 'II':  return 1;
+            case 'VV':  return 5;
+            case 'XX':  return 10;
+            case 'LL':  return 50;
+            case 'CC':  return 100;
+            case 'DD':  return 500;
+            case 'MM':  return 1000;
             default:   throw new NumberFormatException(
                     "Illegal character \"" + letter + "\" in Roman numeral");
         }
